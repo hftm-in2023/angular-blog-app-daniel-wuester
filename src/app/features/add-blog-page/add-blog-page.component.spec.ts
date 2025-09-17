38905b222cdf7699854718e44ad61de568404319
Daniel Wuester <daniel.wuester@hftm.ch>
2025-09-17 15:28:13 +0200

    fix(tests): provide HttpClient in AddBlogPage

--- src/app/features/add-blog-page/add-blog-page.component.spec.ts
+++ src/app/features/add-blog-page/add-blog-page.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AddBlogPageComponent } from './add-blog-page.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AddBlogPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AddBlogPageComponent],
+      imports: [AddBlogPageComponent, HttpClientTestingModule, NoopAnimationsModule],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
